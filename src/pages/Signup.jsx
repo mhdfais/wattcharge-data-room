08863468c8c8fdb0c2594ee8b1b61227365030e9
Mohammed Fais TM <mhdfaistm@gmail.com>
2025-12-16 17:51:17 +0530

    added vercel.json

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -6,7 +6,7 @@ import gif from "../assets/gif.mp4";
 const Signup = () => {
   return (
     <>
-      {/* <video className="absolute bottom-2 right-65" src={gif} autoPlay muted loop></video> */}
+      <video className="absolute bottom-2 right-65" src={gif} autoPlay muted loop></video>
       <Header isLoggedIn={false} />
 
       <main className="flex justify-center pt-16">
